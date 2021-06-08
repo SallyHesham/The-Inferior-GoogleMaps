278c116ab87374e65c66f24ea30d54f52c43f1e1
salwa733 <85217207+salwa733@users.noreply.github.com>
2021-06-08 08:23:12 +0200

    distance2

--- distance/distance.cpp
+++ distance/distance.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-double distance(double lat,double lon)
+
+extern int first_point = 0;
+double total_distance = 0;
+double distance(double lat, double lon)
 {
 	double dist[2][2];
 	int R = 6371 * 1000; //Radius of Earth in meter
@@ -9,32 +12,45 @@ double distance(double lat,double lon)
 	double lon_rad = lon * PI / 180;
 	dist[1][0] = lat_rad;
 	dist[1][1] = lon_rad;
-	//dist[0][0] = 38.898556 * PI / 180; to test.
-	//dist[0][1] = -77.037852 * PI / 180; to test.
-	double difference_lat = (dist[1][0] - dist[0][0]);
-	double difference_lon = (dist[1][1] - dist[0][1]);
-	double lat1 = dist[0][0];
-	double lon1 = dist[0][1];
-	double lat2 = dist[1][0];
-	double lon2 = dist[1][1];
-	double a = sin(difference_lat / 2) * sin(difference_lat / 2) + cos(lat1) * cos(lat2) *
-		sin(difference_lon / 2) * sin(difference_lon / 2);
-	double c = 2 * asin(sqrt(a));
-	double distance = R * c;
-	double total_distance = 0;
-	//if(time == 0){
-	//{
-	//	distance = 0;
-	//	total_distance += the_distance;
-	//	dist[0][0] = dist[1][0];
-	//	dist[0][1] = dist[1][1];
-	//}
-	//else
-	//{
-	total_distance += distance;
-	dist[0][0] = dist[1][0];
-	dist[0][1] = dist[1][1];
-	//}
+
+	if (first_point == 0)
+	{
+		first_point = 1;
+		dist[0][0] = dist[1][0];
+		dist[0][1] = dist[1][1];
+		double difference_lat = (dist[1][0] - dist[0][0]);
+		double difference_lon = (dist[1][1] - dist[0][1]);
+		double lat1 = dist[0][0];
+		double lon1 = dist[0][1];
+		double lat2 = dist[1][0];
+		double lon2 = dist[1][1];
+		double a = sin(difference_lat / 2) * sin(difference_lat / 2) + cos(lat1) * cos(lat2) *
+			sin(difference_lon / 2) * sin(difference_lon / 2);
+		double c = 2 * asin(sqrt(a));
+		double the_distance = R * c;
+
+		total_distance += the_distance;
+
+	}
+	else
+	{
+		double difference_lat = (dist[1][0] - dist[0][0]);
+		double difference_lon = (dist[1][1] - dist[0][1]);
+		double lat1 = dist[0][0];
+		double lon1 = dist[0][1];
+		double lat2 = dist[1][0];
+		double lon2 = dist[1][1];
+		double a = sin(difference_lat / 2) * sin(difference_lat / 2) + cos(lat1) * cos(lat2) *
+			sin(difference_lon / 2) * sin(difference_lon / 2);
+		double c = 2 * asin(sqrt(a));
+		double the_distance = R * c;
+
+		total_distance += the_distance;
+		dist[0][0] = dist[1][0];
+		dist[0][1] = dist[1][1];
+	}
+
+
 	printf(" total_distance = %f", total_distance);
 	return total_distance;
 
@@ -42,6 +58,9 @@ double distance(double lat,double lon)
 int main()
 {
 
-	distance(38.8971471, -77.043934);
+	distance(38, 10);
+	distance(38, 20);
+	distance(38, 30);
+	distance(38, 40);
 
 }
